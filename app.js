14f6274a6696285ba9131d4d10dad326b3587300
Raúl <l17121081@morelia.tecnm.mx>
2021-11-17 19:46:15 -0600

    Raúl.17/11/21
    codigo de conexion a base de datos y primeras lineas en index y login

--- app.js
+++ app.js
@@ -14,10 +14,27 @@ dotenv.config({path:'./env/.env'})
 app.use('/resources', express.static('public'));
 app.use('/resources', express.static(__dirname + '/public'));
 
-console.log(__dirname);
+//5- Establecemos el motor de plantillas ejs
+//app.set('view engine', 'ejs');
 
+//6- Invocamos a bcryptjs
+//const session = require('bcryptjs');
+
+//7Var de session
+//const session = require('express-session');
+//app.use(session({
+    //secret:'seceret',
+    //resave: true,
+    //saveUninitialized:TextTrackCue
+//}));
+
+
+//8- Invocamos al modulo de la conexion de la BD
+//const connection = require('./database/db');
+
+//9- Estableciendo las rutas
 app.get('/', (req, res)=>{
-    res.send('Hola perrito');
+    res.render('index.ejs');
 })
 
 app.listen(3000, (req, res)=>{
