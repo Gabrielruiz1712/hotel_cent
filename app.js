bd9824cd8defbf908b95e96463ee75532387e022
Raúl <l17121081@morelia.tecnm.mx>
2021-11-26 01:21:47 -0600

    Raúl.26/11/21
    Complementar 4 vistas con navbar personalizado y termino de vista inicial

--- app.js
+++ app.js
@@ -51,6 +51,45 @@ app.get('/register', (req, res)=>{
     res.render('register.ejs');
 })
 
+//13- Ruta reservaciones
+app.get('/reservacion', (req, res)=>{
+    res.render('reservacion.ejs');
+})
+
+//13- Ruta reservaciones
+app.get('/registrar', (req, res)=>{
+    res.render('registrar.ejs');
+})
+
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING IN http://localhost:3000');
 })
+
+
+
+// $(document).ready(function() {
+//     $(".search").keyup(function () {
+//       var searchTerm = $(".search").val();
+//       var listItem = $('.results tbody').children('tr');
+//       var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+      
+//     $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+//           return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+//       }
+//     });
+      
+//     $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+//       $(this).attr('visible','false');
+//     });
+  
+//     $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+//       $(this).attr('visible','true');
+//     });
+  
+//     var jobCount = $('.results tbody tr[visible="true"]').length;
+//       $('.counter').text(jobCount + ' item');
+  
+//     if(jobCount == '0') {$('.no-result').show();}
+//       else {$('.no-result').hide();}
+//             });
+//   });
